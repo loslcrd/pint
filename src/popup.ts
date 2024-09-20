554618da6b759c0a16cbc7c175be3a145e2118a4
Louis LECARDONNEL <louisdorsay@gmail.com>
2024-09-20 10:01:26 +0200

    add Real-Debrid API key input&save
    working for real-debrid already (still need to test edge cases)

--- src/popup.ts
+++ src/popup.ts
@@ -1,10 +1,17 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const { realDebridApiKey } = await browser.storage.local.get('realDebridApiKey');
+  if (realDebridApiKey) {
+    (document.getElementById('apiKey') as HTMLInputElement).value = realDebridApiKey;
+  }
+});
+
 document.getElementById('searchHash')?.addEventListener('click', () => {
   console.log('button clicked');
   // Send a message to the background script to search for hash
   browser.runtime.sendMessage({action: 'searchForHash'});
 });
 
-/*document.getElementById('saveApiKey')?.addEventListener('click', () => {
+document.getElementById('saveApiKey')?.addEventListener('click', () => {
   const apiKey = (document.getElementById('apiKey') as HTMLInputElement).value;
   if (apiKey) {
     browser.storage.local
@@ -16,4 +23,4 @@ document.getElementById('searchHash')?.addEventListener('click', () => {
         console.error('Error saving API Key:', error);
       });
   }
-});*/
+});
